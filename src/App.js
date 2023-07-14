2120c35eee64dca26b25d62dedfd28f30eb06611
Trilok Rana <58104039+Trilokrana@users.noreply.github.com>
2023-07-14 19:45:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,280 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Design">
+        <div className="Side-bar">
+          <div className="logo">
+            <h1 className="chaabi">Chaabi</h1>
+          </div>
+          <div className="DashBoard">
+            <div className="menu">Dashboard</div>
+            <div className="Training">Training</div>
+            <div className="User">User</div>
+            <div className="Analytics">Analytics</div>
+            <div className="MyAccount">My Account</div>
+            <div className="Support">Support</div>
+          </div>
+        </div>
+        <div className="chart">
+          <div className="header">
+            <div className="heading">
+              <h1>Hello, Trilok Rana</h1>
+            </div>
+            <div className="subheading">
+              <p className="Summary">Following is Your Organization's Performance Summary</p>
+            </div>
+          </div>
+          <div className="Performance">
+            <div className="PerformanceChart">
+              <div className="chart-1">
+                In Training Workers
+                <div className="Black">
+                  <div className="number-1">3,354</div>
+                  <div className="Groups"></div>
+                </div>
+                <div className="percent-1">20%
+                  <div className="label-1">234</div>
+                  <div className="Graph">
+                    <div className="graph-1">M</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">W</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">F</div>
+                    <div className="graph-1">S</div>
+                    <div className="graph-1">S</div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <div className="PerformanceChart-2">
+              <div className="chart-2">
+                Video Watch-time (hrs)
+                <div className="number-2">2,443</div>
+                <div className="percent-2">20%
+                  <div className="label-2">435</div>
+                  <div className="Graph">
+                    <div className="graph-1">M</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">W</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">F</div>
+                    <div className="graph-1">S</div>
+                    <div className="graph-1">S</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="PerformanceChart">
+              <div className="chart-1">
+                % Workers Passing Quiz
+                <div className="number-3">95%</div>
+                <div className="percent-1">20%
+                  <div className="label-1">24%</div>
+                  <div className="Graph">
+                    <div className="graph-1">M</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">W</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">F</div>
+                    <div className="graph-1">S</div>
+                    <div className="graph-1">S</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="PerformanceChart">
+              <div className="chart-1">
+                Avgerage Days Taken
+                <div className="number-4">06</div>
+                <div className="percent-1">20%
+                  <div className="label-1">3</div>
+                  <div className="Graph">
+                    <div className="graph-1">M</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">W</div>
+                    <div className="graph-1">T</div>
+                    <div className="graph-1">F</div>
+                    <div className="graph-1">S</div>
+                    <div className="graph-1">S</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="CIRCLE">
+            <div className="Status">
+              <div className="Chapter">
+                Chapter Wise Status
+                <div className="Circle-graph">
+                  <div className="small-circle">
+                    <div className="content">Finished Training</div>
+                  </div>
+                </div>
+              </div>
+              <div className="Value">
+                <li className="value-1">Finished Training</li>
+                <li className="value-2">Chapter B</li>
+                <li className="value-3">Haven't Start Yet</li>
+                <li className="value-4">Chapter A</li>
+                <li className="value-5">Chapter C</li>
+              </div>
+            </div>
+            <div className="Big">
+              <div className="bar">Last 14 Days Active Worker In Training
+                <div className="last">Last 14 Days</div>
+              </div>
+              <div className="May">400</div>
+              <div className="May">300 </div>
+              <div className="May">200</div>
+              <div className="May">100 </div>
+              <div className="May-1">0</div>
+              <div className="Colors">
+                <div className="Line-1">
+                  <div className="small-line">
+                  </div>
+                </div>
+                <div className="Line-2">
+                  <div className="small-line-2"></div>
+                </div>
+                <div className="Line-3">
+                  <div className="small-line-2"></div>
+                </div>
+                <div className="Line-4">
+                  <div className="small-line-4"></div>
+                </div>
+                <div className="Line-5">
+                  <div className="small-line-5"></div>
+                </div>
+                <div className="Line-6">
+                  <div className="small-line-6"></div>
+                </div>
+                <div className="Line-7">
+                  <div className="small-line-7"></div>
+                </div>
+                <div className="Line-8">
+                  <div className="small-line"></div>
+                </div>
+                <div className="Line-9">
+                  <div className="small-line-9"></div>
+                </div>
+                <div className="Line-10">
+                  <div className="small-line-10"></div>
+                </div>
+                <div className="Line-11">
+                  <div className="small-line-11"></div>
+                </div>
+                <div className="Line-12">
+                  <div className="small-line-12"></div>
+                </div>
+              </div>
+              <div className="June">
+                <div className="June-1">3 Jun</div>
+                <div className="June-2">4 Jun</div>
+                <div className="June-3">5 Jun</div>
+                <div className="June-4">6 Jun</div>
+                <div className="June-5">7 Jun</div>
+                <div className="June-6">8 Jun</div>
+                <div className="June-7">9 Jun</div>
+                <div className="June-8">10 Jun</div>
+                <div className="June-9">11 Jun</div>
+                <div className="June-10">12 Jun</div>
+                <div className="June-11">13 Jun</div>
+                <div className="June-12">14 Jun</div>
+              </div>
+            </div>
+          </div>
+          <div className="UTF">
+            <div className="Prettier">
+              <div className="BlackBox">Monthly Training Activity
+                <p className="LF">16% more enrollees this month</p>
+              </div>
+              <div className="JavaScript">
+                <li className="list">Course A</li>
+                <p className="Course">23 Workers Took This Course This Week</p>
+                <li className="list">Course B</li>
+                <p className="Course">253 Workers Took This Course This Week</p>
+                <li className="list">Course C</li>
+                <p className="Course">25 Workers Took This Course This Week</p>
+                <li className="list">Course D</li>
+                <p className="Course">197 Workers Took This Course This Week</p>
+              </div>
+            </div>
+            <div className="Spaces">
+              <div className="DTS">Quiz Passing %
+              <div className="Game">
+                <div className="gola"></div>
+                <li className="passed">Passed
+                    <p className="sena">1423 Workers</p>
+                </li>
+                <li className="failed">Failed
+                  <p className="sena">134 Workers</p>
+                </li>
+              </div>
+              </div>
+            </div>
+            
+            <div className="final">
+              <div className="Search">Daily Training Completions
+                <div className="last"></div>
+              </div>
+              <div className="May">160</div>
+              <div className="May">120</div>
+              <div className="May">80</div>
+              <div className="May">40 </div>
+              <div className="May-1">0</div>
+              <div className="Colors">
+                <div className="Line-A">
+                  <div className="small-line">
+                  </div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-2"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-2"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-4"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-5"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-6"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-7"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line"></div>
+                </div>
+                <div className="Line-A">
+                  <div className="small-line-A"></div>
+                </div>
+                
+              </div>
+              <div className="June">
+                <div className="June-1">M</div>
+                <div className="June-2">T</div>
+                <div className="June-3">W</div>
+                <div className="June-4">T</div>
+                <div className="June-5">F</div>
+                <div className="June-6">S</div>
+                <div className="June-7">S</div>
+                <div className="June-8">M</div>
+                <div className="June-9">T</div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
